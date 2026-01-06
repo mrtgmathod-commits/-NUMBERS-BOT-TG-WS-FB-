938787faa5c34f85e5dde3155eabb415771165d8
mrtgmathod-commits <mrtgmathod@gmail.com>
2026-01-06 23:04:32 +0100

    Refactor bot token handling and update welcome message

--- python bot.py
+++ python bot.py
@@ -1,15 +1,15 @@
 import telebot
 
 TOKEN = "8018226164:AAGnhcEigex8LIniGkYWBOui5oXG_LjIq2g"
-bot = telebot.TeleBot(8018226164:AAGnhcEigex8LIniGkYWBOui5oXG_LjIq2g)
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(
         message,
-        "Welcome to NUMBERS BOT\n\n"
-        "⚠️ No spam, no scam, no illegal use.\n"
-        "Use this bot responsibly."
+        "স্বাগতম NUMBERS BOT-এ 🥰\n\n"
+        "⚠️ কোনো স্প্যাম, স্ক্যাম বা অবৈধ ব্যবহার করবেন না।\n"
+        "সঠিকভাবে বট ব্যবহার করুন।"
     )
 
 bot.infinity_polling()
